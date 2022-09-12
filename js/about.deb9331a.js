963ce5f0aeabb809ad7bd2377977a13b6796cccb
xavihigg <xjhiggins@gmail.com>
2022-09-12 17:20:55 -0500

    gh-pages

---
+++ js/about.deb9331a.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkdev"]=self["webpackChunkdev"]||[]).push([[443],{242:function(n,e,u){u.r(e),u.d(e,{default:function(){return f}});var t=u(252);const a={class:"about"},r=(0,t._)("h1",null,"This is an about page",-1),s=[r];function c(n,e){return(0,t.wg)(),(0,t.iD)("div",a,s)}var i=u(744);const o={},d=(0,i.Z)(o,[["render",c]]);var f=d}}]);
+//# sourceMappingURL=about.deb9331a.js.map
